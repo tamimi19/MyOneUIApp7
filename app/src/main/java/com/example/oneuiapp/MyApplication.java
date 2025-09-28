d5bbda7236620cb4c13d0255953b31398607ad32
tamimi19 <mohamad.tamimii.82@gmail.com>
2025-09-28 12:45:40 +0300

    Create MyApplication.java

---
+++ app/src/main/java/com/example/oneuiapp/MyApplication.java
@@ -0,0 +1,11 @@
+package com.example.oneuiapp;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashHandler.init(this);
+    }
+}
