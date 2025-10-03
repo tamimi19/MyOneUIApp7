c7d552fc8f38e300e8b95b0bcaa532bafa13453b
tamimi19 <mohamad.tamimii.82@gmail.com>
2025-10-03 10:33:24 +0300

    Update MyApplication.java

--- app/src/main/java/com/example/oneuiapp/MyApplication.java
+++ app/src/main/java/com/example/oneuiapp/MyApplication.java
@@ -3,10 +3,9 @@ package com.example.oneuiapp;
 import android.app.Application;
 
 public class MyApplication extends Application {
-
     @Override
     public void onCreate() {
         super.onCreate();
-        SettingsHelper.initializeFromSettings(this);
+        CrashHandler.init(this);
     }
 }
