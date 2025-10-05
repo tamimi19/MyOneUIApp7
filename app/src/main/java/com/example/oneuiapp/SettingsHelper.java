d926fe14730b46f3ae34e5451be59cc329da70e1
tamimi19 <mohamad.tamimii.82@gmail.com>
2025-10-05 12:32:03 +0300

    Update SettingsHelper.java

--- app/src/main/java/com/example/oneuiapp/SettingsHelper.java
+++ app/src/main/java/com/example/oneuiapp/SettingsHelper.java
@@ -48,6 +48,7 @@ public class SettingsHelper {
 
     public void applyTheme() {
         int mode = getThemeMode();
+
         switch (mode) {
             case THEME_LIGHT:
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
@@ -57,7 +58,8 @@ public class SettingsHelper {
                 break;
             case THEME_SYSTEM:
             default:
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                AppCompatDelegate.setDefaultNightMode(
+                        AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
                 break;
         }
     }
